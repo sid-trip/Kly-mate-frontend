ac12f3fce4c0b1b3b0f562a5a80567033ddbcd40
Siddhant <siddhant.tripathy1@gmail.com>
2025-04-23 18:22:50 +0530

    centered logo, reduced size

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ st.set_page_config(
 
 logo_path = Path(__file__).parent / "Kly-Mate.png" 
 
-if logo_path.exists():
-    st.image(str(logo_path)) # Adjust width as needed
-else:
-    st.warning("Logo file not found. Please place it in the same directory as the script.")
+# if logo_path.exists():
+#     st.image(str(logo_path)) # Adjust width as needed
+# else:
+#     st.warning("Logo file not found. Please place it in the same directory as the script.")
 
 # --- App Title and Description ---
 st.title("Kly-mate Weather Dashboard")
@@ -29,7 +29,7 @@ st.markdown(
     }
     </style>
     <div class="center-image">
-        <img src="E:\KLY-MATE\Klymate_frontend\Kly-Mate.png" width="500"/>
+        <img src="E:\KLY-MATE\Klymate_frontend\Kly-Mate.png" width="300"/>
     </div>
     """, 
     unsafe_allow_html=True
